18c0123adb0837d06970d6e90e94607f5cb33b2e
thomas <dui1cuo@126.com>
2017-08-08 15:15:44 +0800

    [Mod]minor fix

---
+++ TouchIDDemo/TouchIDDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchIDDemo
+//
+//  Created by admin on 2017/8/8.
+//  Copyright © 2017年 fuxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchIDDemo/TouchIDDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TouchIDDemo
+//
+//  Created by admin on 2017/8/8.
+//  Copyright © 2017年 fuxing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
